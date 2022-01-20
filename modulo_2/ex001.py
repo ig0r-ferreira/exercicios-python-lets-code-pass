8ed0f4408d9b44609a18927a2355b6509e175322
Igor Ferreira <igor-freitas10@hotmail.com>
2022-01-20 12:28:18 -0300

    Realizada alteração na mensagem final do código.

--- modulo_2/ex001.py
+++ modulo_2/ex001.py
@@ -9,4 +9,4 @@ numeros = list(range(10))
 pares = [num for num in numeros if num % 2 == 0]
 
 print(f'Lista: {numeros}\n'
-      f'Há {len(pares)} numéros pares na lista.')
+      f'Há {len(pares)} números pares na lista.')
